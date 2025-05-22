08d7cc6d7251466e0baacd426ad60a1577fb50e1
Kunal <kunalpandit5200@gmail.com>
2025-05-22 18:40:30 +0530

    resolve server constants

--- src/assets/api/serverConstants.ts
+++ src/assets/api/serverConstants.ts
@@ -1,8 +1,8 @@
 // const baseUrl = "https://api.vroar.ai";
-const baseUrl = "https://dev.api.vroar.ai";
+// const baseUrl = "https://dev.api.vroar.ai";
 // const baseUrl = "https://devapi.mytreks.ai";
 // const baseUrl = "https://api.dev.mytreks.ai/";
-// const baseUrl = "https://uatapi.mytreks.ai";
+const baseUrl = "https://uatapi.mytreks.ai";
 const stripeURL = "https://api.stripe.com/v1";
 export const serverApiUrl = {
   admin: `${baseUrl}/admin/api`,
