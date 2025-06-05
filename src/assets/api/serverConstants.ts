01b86cdd97323b8e366679371ce9c4d30971a4d2
Kunal Sharma <kunalpandit5200@gmail.com>
2025-06-05 12:17:06 +0530

    remove unused urls

--- src/assets/api/serverConstants.ts
+++ src/assets/api/serverConstants.ts
@@ -1,19 +1,10 @@
-// const baseUrl = "https://api.mytreks.ai";
-// const baseUrl = "https://dev.api.vroar.ai";
-// const baseUrl = "https://devapi.mytreks.ai";
-
 const baseUrl = "https://uatapi.mytreks.ai";
 const stripeURL = "https://api.stripe.com/v1";
-// const authenticationUrl = "http://localhost:8085";
-// const contentUrl = "http://localhost:8084";
-// const userUrl = "http://localhost:8086";
+
 export const serverApiUrl = {
   authentication: `${baseUrl}/api`,
   user: `${baseUrl}/user/api`,
   internship: `${baseUrl}/internship/api`,
   content: `${baseUrl}/content/api`,
   stripeCustomer: `${stripeURL}/api`,
-  // content: `${contentUrl}/content/api`,
-  // user: `${userUrl}/api`,
-  // authentication: `${authenticationUrl}/api`,
 };
