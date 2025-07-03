e1ad53d59fb71d265a2f0882932a36ddd0cf0093
Kunal Sharma <kunalpandit5200@gmail.com>
2025-07-03 17:40:09 +0530

    add webiste

---
+++ src/components/Home/TestimonialBanner/TestimonialCard.tsx
@@ -0,0 +1,12 @@
+import { Card } from '@mui/material'
+import React from 'react'
+
+const TestimonialCard = () => {
+  return (
+    <Card>
+        
+    </Card>
+  )
+}
+
+export default TestimonialCard
